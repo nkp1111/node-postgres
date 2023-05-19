38616ba9c573c8d6f538b24d613bf08fcc65f27b
nkp1111 <neerajp4321@gmail.com>
2023-05-19 12:06:39 +0530

    feat: use sequelize to create and read task

--- index.js
+++ index.js
@@ -1,60 +1,55 @@
+const { DataTypes } = require("sequelize");
 const dbConnection = require("./db/connection")
 
-const client = dbConnection()
+dbConnection().then(async data => {
+  const sequelize = data
 
-const dropTable = async (table) => {
-  // drop table with given name 
-  // params: table(array) - ["table_name"]
-  const dropQuery = "DROP TABLE $1::text;"
-  const res = await client.query(dropQuery, table)
-  return res
-}
+  const dropTable = async () => {
+    try {
+      await Task.drop();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // dropTable()
 
-const createTable = async () => {
-  // create table of name 'task' 
-  // with id,task_name,completed,time as columns
-  const createQuery = `CREATE TABLE task(
-    id SERIAL PRIMARY KEY, 
-    task_name VARCHAR(200) NOT NULL, 
-    completed BOOL NOT NULL DEFAULT false, 
-    time TIMESTAMP);`
-  const res = await client.query(createQuery)
-  return res
-}
+  const Task = sequelize.define('Task', {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    task_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    // Other model options go here
+    freezeTableName: true,
+  });
 
-const readTable = async () => {
-  // read task table and returns all the rows value
-  const selectQuery = "SELECT * FROM task;"
-  const res = await client.query(selectQuery)
-  return res
-}
 
-const insertIntoTable = async (task_name, time, status) => {
-  // insert new task into table
-  // params: task_name (string) - name of the task
-  //         time (timestamp) - task starting time
-  //         status (boolean) - task status whether completed or not
-  const taskItem = [task_name, time, status]
-  const insertQuery = "INSERT INTO task (task_name, time, completed) VALUES ($1::text, $2::timestamp, $3::bool);"
-  const res = await client.query(insertQuery, taskItem)
-  return res
-}
+  const createTask = async () => {
+    await sequelize.sync()
+    const task = await Task.create({ task_name: "modifying code", completed: false });
+    console.log(task.id);
+  }
 
-const updateTable = async (id, status) => {
-  // update task of given id into new status
-  // params: id(int) - id of the task
-  //         status (bool) - either true or false
-  const updateItem = [id, status]
-  const updateQuery = "UPDATE task set completed=$2::bool where id=$1::int"
-  const res = await client.query(updateQuery, updateItem)
-  return res
-}
+  const findAllTask = async () => {
+    try {
+      await sequelize.sync()
+      const tasks = await Task.findAll();
+      console.log(tasks)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-const deleteFromTable = async (id) => {
-  // delete task from table of given id
-  // params: id (int) - id of task
-  const deleteItem = [id]
-  const deleteQuery = "DELETE FROM task where id = $1::int"
-  const res = await client.query(deleteQuery, deleteItem)
-  return res
-}
+  createTask()
+  findAllTask()
+})
