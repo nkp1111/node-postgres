9612bacf6b8b614fe8a8bcdd37b34adf53acf64e
nkp1111 <neerajp4321@gmail.com>
2023-05-19 12:34:19 +0530

    feat: add express and route for all task view

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize")
 const dbConnection = require("./db/connection")
+const express = require("express")
+const app = express()
+
+const port = 3000
 
 dbConnection().then(async data => {
   const sequelize = data
@@ -11,7 +15,6 @@ dbConnection().then(async data => {
       console.log(error)
     }
   }
-  // dropTable()
 
   const Task = sequelize.define('Task', {
     // Model attributes are defined here
@@ -33,23 +36,48 @@ dbConnection().then(async data => {
     freezeTableName: true,
   });
 
-
-  const createTask = async () => {
-    await sequelize.sync()
-    const task = await Task.create({ task_name: "modifying code", completed: false });
-    console.log(task.id);
-  }
-
-  const findAllTask = async () => {
+  const deleteTask = async (id) => {
     try {
-      await sequelize.sync()
-      const tasks = await Task.findAll();
-      console.log(tasks)
+      await Task.destroy({
+        where: {
+          id
+        }
+      });
     } catch (error) {
       console.log(error)
     }
   }
 
-  createTask()
-  findAllTask()
+  app.get("/", async (req, res) => {
+    // get all tasks from database
+    try {
+      await sequelize.sync()
+      const tasks = await Task.findAll();
+      console.log(tasks)
+      res.send(tasks)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  app.get("/new/:task_name", async (req, res) => {
+    const { task_name } = req.params
+    try {
+      await sequelize.sync()
+      const task = await Task.create({ task_name, completed: false });
+      console.log(task.id);
+      res.send(task)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  app.listen(port, (e) => {
+    console.log("App listening on port " + port)
+  })
 })
+
+
+
+
+
